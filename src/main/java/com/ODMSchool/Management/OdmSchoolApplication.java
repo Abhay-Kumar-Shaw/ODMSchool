80a401af68579eb64a6291376337c20ec5391869
AbhayShaw <abhayshaw147@gmail.com>
2025-07-13 10:04:16 +0530

    Initial commit

---
+++ src/main/java/com/ODMSchool/Management/OdmSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.ODMSchool.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdmSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdmSchoolApplication.class, args);
+	}
+
+}
